68a0b7e598e67b17f4715e42564b8ed15fe8c8aa
11ume <horacioriverods@gmail.com>
2020-10-19 17:27:31 -0300

    add router flow fixes

--- dev/router.ts
+++ dev/router.ts
@@ -1,4 +1,4 @@
-import router, { ContextRoute, get, post, put } from 'router'
+import { ContextRoute, whitNamespace, get, post, put } from 'router'
 import { json } from 'recibers'
 
 type UserBody = {
@@ -14,7 +14,8 @@ const getById = (ctx: ContextRoute<UserByIdParams>) => ctx.params.id
 const create = async (ctx: ContextRoute) => json<UserBody>(ctx)
 const update = async (ctx: ContextRoute) => json<UserBody>(ctx)
 
-const r = router(
+const v1 = whitNamespace('/api')
+const r = v1(
     get('/users', getAll)
     , get('/users/:id', getById)
     , post('/users', create)
