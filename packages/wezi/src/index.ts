d26916be69ecc7e8109f5e273cea5fff9fa66a46
11ume <horacioriverods@gmail.com>
2020-10-30 20:27:07 -0300

    add listener fn

--- packages/wezi/src/index.ts
+++ packages/wezi/src/index.ts
@@ -1,13 +1,19 @@
-import { IncomingMessage, ServerResponse } from 'http'
+import http, { RequestListener, IncomingMessage, ServerResponse } from 'http'
 import { Context, Handler } from 'wezi-types'
 import { mergeHandlers } from './utils'
 import composer from 'wezi-composer'
 
-// default error handler
 export const errorHandler = (ctx: Context) => {
     ctx.res.statusCode = ctx.error.statusCode || 500
     ctx.res.end()
 }
+
+export const listen = (run: RequestListener, port: number) => new Promise((resolve, reject) => {
+    const server = http.createServer(run)
+    server.on('listening', resolve)
+    server.on('error', reject)
+    return server.listen(port)
+})
 
 const run = (handler: Handler | Handler[], ...handlers: Handler[]) => (errHandler: Handler = errorHandler) => {
     const mergedHandlers = mergeHandlers(handler, handlers)
@@ -23,5 +29,6 @@ const run = (handler: Handler | Handler[], ...handlers: Handler[]) => (errHandle
         dispatch(context)
     }
 }
+
 
 export default run
