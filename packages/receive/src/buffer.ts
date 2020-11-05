1bd4bd19fd9be0d5ef0625210998f927ce6503bd
11ume <horacioriverods@gmail.com>
2020-11-05 11:57:09 -0300

    add json parse cache

--- packages/receive/src/buffer.ts
+++ packages/receive/src/buffer.ts
@@ -1,19 +1,25 @@
+import { IncomingMessage } from 'http'
 import { Context } from 'wezi-types'
 import createError from 'wezi-error'
 import getRawBody, { RawBodyError } from 'raw-body'
 
 type ParseBody = {
     context: Context
-    , length: string
     , limit: string | number
+    , length: string
     , encoding: getRawBody.Encoding
+    , rawBodyCache: WeakMap<IncomingMessage, unknown>
 }
 
-export const parseBody = ({ context, length, limit, encoding }: ParseBody) => getRawBody(context.req, {
+export const parseBody = ({ context, limit, length, encoding, rawBodyCache }: ParseBody) => getRawBody(context.req, {
     limit
     , length
     , encoding
 })
+    .then((rawBody) => {
+        rawBodyCache.set(context.req, rawBody)
+        return rawBody
+    })
     .catch((err: RawBodyError) => {
         if (err.type === 'entity.too.large') {
             throw createError(413, `Body exceeded ${limit} limit`, err)
