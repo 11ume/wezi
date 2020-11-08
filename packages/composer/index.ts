38a4bdbd843e4cc391a38034d93374565256519e
11ume <horacioriverods@gmail.com>
2020-11-08 07:38:52 -0300

    remove error test

--- packages/composer/index.ts
+++ packages/composer/index.ts
@@ -1,6 +1,5 @@
 import { Context, Handler } from 'wezi-types'
 import { send } from 'wezi-send'
-import { HttpError } from 'wezi-error'
 
 type Dispatch = (context: Context, payload: unknown) => void
 
@@ -15,9 +14,6 @@ const execute = async (context: Context, handler: Handler, payload: unknown) => 
         if (val !== undefined) {
             send(context, context.res.statusCode, val)
             return
-        }
-        if (val instanceof HttpError) {
-            context.next(val)
         }
     } catch (err) {
         context.next(err)
