4517bb8131d660bd5b0c7e40afb83bd2f1223739
11ume <horacioriverods@gmail.com>
2020-11-13 19:46:47 -0300

    add safe shutdown to listen

--- packages/wezi/src/core.ts
+++ packages/wezi/src/core.ts
@@ -1,9 +1,11 @@
 import http, { RequestListener, IncomingMessage, ServerResponse } from 'http'
-import { send } from 'wezi-send'
-import { Context, Handler } from 'wezi-types'
 import composer from 'wezi-composer'
+import { Context, Handler } from 'wezi-types'
+import { send } from 'wezi-send'
 
-export const errorHandler = (ctx: Context) => {
+type SafeShutdownHandler = (code: number) => void
+
+const defaultErrorHandler = (ctx: Context) => {
     const status = ctx.error.statusCode || 500
     if (ctx.error.message) {
         send(ctx, status, {
@@ -15,14 +17,7 @@ export const errorHandler = (ctx: Context) => {
     send(ctx, status)
 }
 
-export const listen = (run: RequestListener, port: number) => new Promise((resolve, reject) => {
-    const server = http.createServer(run)
-    server.on('listening', resolve)
-    server.on('error', reject)
-    return server.listen(port)
-})
-
-const run = (...handlers: Handler[]) => (errHandler: Handler = errorHandler) => {
+const run = (...handlers: Handler[]) => (errorHandler: Handler = defaultErrorHandler) => {
     return (req: IncomingMessage, res: ServerResponse) => {
         const dispatch = composer(true, ...handlers)
         const context: Context = {
@@ -30,11 +25,29 @@ const run = (...handlers: Handler[]) => (errHandler: Handler = errorHandler) => 
             , res
             , next: null
             , error: null
-            , errorHandler: errHandler
+            , errorHandler
         }
 
         dispatch(context)
     }
 }
+
+const safeShutdown = (fn: SafeShutdownHandler) => {
+    process.on('exit', (code: number) => fn && fn(code))
+    process.on('SIGINT', (_, code: number) => process.exit(code))
+    process.on('SIGTERM', (_, code: number) => process.exit(code))
+}
+
+export const listen = (handler: RequestListener, port: number, shutdownHandler?: SafeShutdownHandler) => new Promise((resolve, reject) => {
+    const server = http.createServer(handler)
+    safeShutdown((code) => {
+        server.close()
+        shutdownHandler(code)
+    })
+    server.on('listening', resolve)
+    server.on('error', reject)
+    server.listen(port)
+    return server
+})
 
 export default run
