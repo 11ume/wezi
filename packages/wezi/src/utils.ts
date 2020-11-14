72151af0f7ad6a49dd1f1a6e3b2f2e4293fe8109
11ume <horacioriverods@gmail.com>
2020-11-14 13:09:12 -0300

    add more error tests

--- packages/wezi/src/utils.ts
+++ packages/wezi/src/utils.ts
@@ -1,6 +1,6 @@
 import { Context } from 'wezi-types'
 
-export const isDev = () => process.env.NODE_ENV === 'development'
+export const isProd = () => process.env.NODE_ENV === 'production'
 export const status = (context: Context, statusCode: number) => {
     context.res.statusCode = statusCode
 }
--- tests/error.test.ts
+++ tests/error.test.ts
@@ -8,7 +8,36 @@ type ErrorPayload = {
     message: string
 };
 
-test('create and return http error', async (t) => {
+test.before('pass to next http error in production', async (t) => {
+    process.env.NODE_ENV = 'production'
+    const url = await server((c: Context) => c.next(createError(420)))
+    const res = await fetch(url)
+    const body: string = await res.text()
+
+    process.env.NODE_ENV = ''
+    t.is(res.status, 420)
+    t.is(body, '')
+})
+
+test('pass to next empty error', async (t) => {
+    const url = await server((c: Context) => c.next(new Error()))
+    const res = await fetch(url)
+    const body: ErrorPayload = await res.json()
+
+    t.is(res.status, 500)
+    t.is(body.message, 'unknown')
+})
+
+test('pass to next random error whit message', async (t) => {
+    const url = await server((c: Context) => c.next(new Error('opps')))
+    const res = await fetch(url)
+    const body: ErrorPayload = await res.json()
+
+    t.is(res.status, 500)
+    t.is(body.message, 'opps')
+})
+
+test('create and pass to next http error', async (t) => {
     const url = await server((c: Context) => c.next(createError(420)))
     const res = await fetch(url)
     const body: ErrorPayload = await res.json()
@@ -17,7 +46,7 @@ test('create and return http error', async (t) => {
     t.is(body.message, 'Enhance Your Calm')
 })
 
-test('create and return http error whit custom status code and message', async (t) => {
+test('create and pass to next http error whit custom status code and message', async (t) => {
     const url = await server((c: Context) => c.next(createError(418, 'Im a teapot')))
     const res = await fetch(url)
     const body: ErrorPayload = await res.json()
@@ -26,7 +55,7 @@ test('create and return http error whit custom status code and message', async (
     t.is(body.message, 'Im a teapot')
 })
 
-test('create and return http error multiple handlers', async (t) => {
+test('create and pass to next http error multiple handlers', async (t) => {
     const url = await server((c: Context) => c.next(), (c: Context) => c.next(createError(420)), () => 'not')
     const res = await fetch(url)
     const body: ErrorPayload = await res.json()
