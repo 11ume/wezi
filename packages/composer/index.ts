800f5f04b3c51fd2299402cab8d166955acace85
11ume <horacioriverods@gmail.com>
2020-11-15 12:48:05 -0300

    create new context when dispatch is invoked

--- packages/composer/index.ts
+++ packages/composer/index.ts
@@ -3,6 +3,11 @@ import { send } from 'wezi-send'
 
 type Dispatch = (context: Context, payload: unknown) => void
 
+// end response if all higher-order handlers are executed, and none of them have ended the response
+const end = (main: boolean, context: Context) => main && context.res.end()
+
+const createContext = <T>(context: Context, newContext: T) => Object.assign(context, newContext)
+
 const execute = async (context: Context, handler: Handler, payload: unknown) => {
     try {
         const val = await handler(context, payload)
@@ -20,22 +25,18 @@ const execute = async (context: Context, handler: Handler, payload: unknown) => 
     }
 }
 
-const createContext = <T>(context: Context, newContext: T) => Object.assign(context, newContext)
-
 const createNext = (context: Context, dispatch: Dispatch) => {
-    return function next(payload: unknown): void {
-        dispatch(context, payload)
+    return function next(payload?: unknown): void {
+        const newContext = createContext(context, {})
+        dispatch(newContext, payload)
     }
 }
-
-// end response if all higher-order handlers are executed, and none of them have ended the response
-const end = (main: boolean, context: Context) => main && context.res.end()
 
 const composer = (main: boolean, ...handlers: Handler[]) => {
     let i = 0
     return function dispatch(context: Context, payload?: unknown): void {
         if (context.res.writableEnded) return
-        if (payload instanceof Error) {
+        if (payload && payload instanceof Error) {
             context.errorHandler(context, payload)
             return
         }
