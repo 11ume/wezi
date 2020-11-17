2560c2cc9b98438247c65d90519fd333d82012ef
11ume <horacioriverods@gmail.com>
2020-11-17 01:09:00 -0300

    remove unused asnyc

--- examples/rest/src/api/bears/handlers.ts
+++ examples/rest/src/api/bears/handlers.ts
@@ -11,21 +11,21 @@ export const getById = ({ params }: ContextRoute<Pick<Bear, 'id'>>) => {
     return null
 }
 
-export const create = async (c: ContextRoute, bear: Bear) => {
+export const create = (c: ContextRoute, bear: Bear) => {
     repository.create(bear)
     send(c, 200, {
         meesage: 'created'
     })
 }
 
-export const update = async (c: ContextRoute, bear: Bear) => {
+export const update = (c: ContextRoute, bear: Bear) => {
     if (repository.update(bear)) return send(c, 200)
     return send(c, 404, {
         message: 'resource not found'
     })
 }
 
-export const remove = async (c: ContextRoute<Pick<Bear, 'id'>>) => {
+export const remove = (c: ContextRoute<Pick<Bear, 'id'>>) => {
     if (repository.remove(c.params.id)) return send(c, 200)
     return send(c, 404, {
         message: 'resource not found'
