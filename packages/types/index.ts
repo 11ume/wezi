ba2c5cf20eea1d9bca0d873b141821479da9807a
11ume <horacioriverods@gmail.com>
2020-12-13 21:00:02 -0300

    fix cycles packages depedecies

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -1,7 +1,24 @@
 import { IncomingMessage, ServerResponse } from 'http'
-import { Send } from 'wezi-send'
-import { Receive } from 'wezi-receive'
-import { Actions } from 'wezi-actions'
+import { Options as GetRawBodyOptions } from 'raw-body'
+import { Readable } from 'stream'
+
+export interface Actions {
+    redirect: (location: string) => void
+}
+
+export interface Send {
+    json: <T>(payload: T, statusCode?: number) => void
+    text: (payload: string | number, statusCode?: number) => void
+    empty: (statusCode?: number) => void
+    stream: (payload: Readable, statusCode?: number) => void
+    buffer: (payload: Buffer, statusCode?: number) => void
+}
+
+export interface Receive {
+    json: <T>(options?: GetRawBodyOptions) => Promise<T>
+    text: (options?: GetRawBodyOptions) => Promise<string>
+    buffer: (options?: GetRawBodyOptions) => Promise<string>
+}
 
 export type ErrorHandler = (context: Context, error: Error) => void
 export interface Context {
