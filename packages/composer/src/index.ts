294fe8598f874d5f17845038b3e108ff5d83b754
11ume <horacioriverods@gmail.com>
2021-01-17 23:38:53 -0300

    fix: missing dependency

--- packages/composer/src/index.ts
+++ packages/composer/src/index.ts
@@ -1,5 +1,5 @@
-import { create, createSigle } from './composer'
 import { endHandler, errorHandler, executeHandler } from 'wezi-lazy-handlers'
+import { create, createSigle } from './composer'
 
 export const composer = create(endHandler, errorHandler, executeHandler)
 export const composerSingle = createSigle(errorHandler, executeHandler)
--- packages/composer/src/utils.ts
+++
@@ -1,3 +0,0 @@
-export const isPromise = (obj: any) => typeof obj.then === 'function'
-export const isProduction = () => process.env.NODE_ENV === 'production'
-
