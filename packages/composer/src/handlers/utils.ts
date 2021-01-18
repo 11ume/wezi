341bdf252b6f88fa282d851ec8ceca9c343d0489
11ume <horacioriverods@gmail.com>
2021-01-18 12:30:25 -0300

    refator: enhanced is promise method type

--- packages/composer/src/handlers/utils.ts
+++ packages/composer/src/handlers/utils.ts
@@ -1,3 +1,3 @@
-export const isPromise = (obj: any): boolean => typeof obj.then === 'function'
+export const isPromise = (value: Partial<Promise<unknown>>): boolean => typeof value.then === 'function'
 export const isProduction = (): boolean => process.env.NODE_ENV === 'production'
 
