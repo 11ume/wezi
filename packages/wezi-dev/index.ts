d8bb0a5f60da9dc08b84d3a98b4652582bf67f23
11ume <horacioriverods@gmail.com>
2021-02-28 19:13:44 -0300

    remove wezi dev

--- packages/wezi-dev/index.ts
+++
@@ -1,1 +0,0 @@
-export { listen } from './src'
--- packages/wezi-dev/rollup.config.js
+++
@@ -1,6 +0,0 @@
-import config from '../../build/builder'
-import { dependencies } from './package.json'
-
-export default config({
-    external: dependencies
-})
