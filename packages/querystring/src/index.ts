c15049629cda1b99b91a7b25202708db2476d0c1
11ume <horacioriverods@gmail.com>
2021-03-10 06:56:43 -0300

    refactor: improve query string types

--- packages/querystring/src/index.ts
+++ packages/querystring/src/index.ts
@@ -5,16 +5,17 @@ type Dict<T> = {
     [K in keyof T]: T[K]
 }
 
-const getQuery = <T>(url: string, index: number, sep?: string, eq?: string): Dict<T> => {
+const getQuery = (url: string, index: number, sep?: string, eq?: string): any => {
     const search = url.substring(index)
     const path = search.substring(1)
-    return querystring.parse(path, sep, eq) as any
+    return querystring.parse(path, sep, eq)
 }
 
-const getQueryString = <T>(url: string, sep?: string, eq?: string): Dict<T> => {
+const getQueryString = <T>(url: string, sep?: string, eq?: string): Partial<Dict<T>> => {
     const index = url.indexOf('?', 1)
     if (index !== -1) return getQuery(url, index, sep, eq)
-    return {} as Dict<T>
+    return {}
 }
 
 export const queryParser = <T>(context: Context, sep?: string, eq?: string): Partial<Dict<T>> => getQueryString(context.req.url, sep, eq)
+
