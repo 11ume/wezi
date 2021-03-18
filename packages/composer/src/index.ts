af3efcece12cc254a9f6cdbacd71378a64589b0a
11ume <horacioriverods@gmail.com>
2021-03-18 14:37:26 -0300

    refactor: change composer handlers import

--- packages/composer/src/index.ts
+++ packages/composer/src/index.ts
@@ -1,6 +1,6 @@
 import { ErrorHandler } from 'wezi-types'
 import { createComposer, createComposerMain } from './composer'
-import * as handlers from './handlers'
+import { endHandler, errorHandler, executeHandler } from './handlers'
 
-export const composer = (errorHandler: ErrorHandler = handlers.errorHandler) => createComposer(errorHandler, handlers.executeHandler)
-export const composerMain = (errorHandler: ErrorHandler = handlers.errorHandler) => createComposerMain(handlers.endHandler, errorHandler, handlers.executeHandler)
+export const composer = (errHandler: ErrorHandler = errorHandler) => createComposer(errHandler, executeHandler)
+export const composerMain = (errHandler: ErrorHandler = errorHandler) => createComposerMain(endHandler, errHandler, executeHandler)
